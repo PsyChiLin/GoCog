0696082bd707e42269b6520b4fa41ab1baa70537
PsyChiLin <psychilinyu@gmail.com>
2018-04-28 17:35:03 +0800

    plotscatter3d

---
+++ Demo3D_newplot.R
@@ -0,0 +1,56 @@
+rm(list = ls())
+library(psych)
+library(plotly)
+library("scatterplot3d")
+library(dplyr)
+source('http://www.sthda.com/sthda/RDoc/functions/addgrids3d.r')
+
+
+dta_3s_rst <- readRDS("../GoCogdata/dta_3s_rst.Rdata")
+# PCA
+rst_pca <- prcomp(x = dta_3s_rst[5:12],center = TRUE,scale. = TRUE)
+#summary(rst_pca)
+# 3d plot
+dta_3s_rst[,14:16] <- predict(rst_pca, newdata=dta_3s_rst)
+#head(dta_3s_rst)
+colnames(dta_3s_rst)[14:16] <- c("PC1","PC2","PC3")
+dta_3s_rst$TF <- ifelse(dta_3s_rst$GoStage == dta_3s_rst$pred_GoStage,
+                        as.character(dta_3s_rst$GoStage),"Error")
+dta_3s_rst$TF <- factor(dta_3s_rst$TF, levels = c("Open","Mid","End","Error"))
+
+shapes = c(1, 19, 2, 4) 
+shapes <- shapes[as.numeric(dta_3s_rst$TF)]
+pdf(file = "../GoCog_Manuscript/FigureTable/圖5_GoStage_3D.pdf", width = 7, height = 7)
+gostage <- scatterplot3d(dta_3s_rst[,14:16], pch = "", grid=TRUE, box=FALSE,label.tick.marks=F,
+              xlab = " ", ylab = " ",zlab = " ")
+addgrids3d(dta_3s_rst[,14:16], grid = c("xy", "xz", "yz"))
+gostage$points3d(dta_3s_rst[,14:16], pch = shapes)
+dev.off()
+#legend("bottom", legend = levels(dta_3s_rst$TF), pch = c(1, 2, 19, 4))
+
+
+dta_4c_rst <- readRDS("../GoCogdata/dta_4c_rst.Rdata")
+aver2 <- aggregate(cbind(End_Both_ACC,Mid_Both_ACC,Open_Both_ACC,
+                         End_Both_RT,Mid_Both_RT,Open_Both_RT)~pred_CogTask,
+                   data = dta_4c_rst, FUN = mean)
+# PCA
+rst_pca <- prcomp(x = dta_4c_rst[5:10],center = TRUE,scale. = TRUE)
+#summary(rst_pca)
+# 3d plot
+dta_4c_rst[,12:14] <- predict(rst_pca, newdata=dta_4c_rst)
+#head(dta_4c_rst)
+colnames(dta_4c_rst)[12:14] <- c("PC1","PC2","PC3")
+dta_4c_rst$TF <- ifelse(dta_4c_rst$CogTask == dta_4c_rst$pred_CogTask,
+                        as.character(dta_4c_rst$CogTask),"Error")
+
+dta_4c_rst$TF <- factor(dta_4c_rst$TF,levels = c("None","Spat","Reas","Calc","Error"))
+
+shapes2 = c(16, 2, 17, 0, 4) 
+shapes2 <- shapes2[as.numeric(dta_4c_rst$TF)]
+pdf(file = "../GoCog_Manuscript/FigureTable/圖5_CogTask_3D.pdf", width = 7, height = 7)
+cogtask <- scatterplot3d(dta_4c_rst[,12:14], pch = "", grid=TRUE, box=FALSE,label.tick.marks=F,
+                         xlab = " ", ylab = " ",zlab = " ")
+addgrids3d(dta_4c_rst[,12:14], grid = c("xy", "xz", "yz"))
+cogtask$points3d(dta_4c_rst[,12:14], pch = shapes2)
+dev.off()
+#legend("bottom", legend = levels(dta_4c_rst$TF), pch = c(16, 2, 5, 22,4))
